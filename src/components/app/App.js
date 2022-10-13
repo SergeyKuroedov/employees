ce592218c642b35981459112e71714c881c7f9ea
SergeyKuroedov <graysonboler@mail.ru>
2022-10-13 01:59:32 -0800

    add layout with components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,21 +4,25 @@ import '../app-info/App-info'
 import AppInfo from '../app-info/App-info';
 import SearchPanel from '../search-panel/Search-panel';
 import AppFilter from '../app-filter/App-filter';
+import Employeeslist from '../employees-list/Employees-list';
+import EmployeesAddForm from '../employees-add-form/Employees-add-form';
 
 
 function App() {
-  return (
-    <div className="App">
-      <AppInfo />
+	return (
+		<div className="App">
+			<AppInfo />
 
-      <div className='search-panel'>
-			<SearchPanel />
-			<AppFilter />.
-      </div>
+			<div className='search-panel'>
+				<SearchPanel />
+				<AppFilter />.
+			</div>
 
-    </div>
+			<Employeeslist />
+			<EmployeesAddForm />
+		</div>
 
-  );
+	);
 }
 
 export default App;
